7ecfd13bddaf306c5ab3871ae3f13cf7cd8452ff
indraneepal06 <indraneepal1@gmail.com>
2026-05-28 11:32:20 +0530

    Fixing error

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,13 @@
 import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <>
+      <Navbar />
       <Hero />
+      <Footer />
     </>
   );
 };
